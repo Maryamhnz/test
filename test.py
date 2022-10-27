6bb9e54bbc19c403eedae199dc84bfa685d9cee1
maryam <maryam.hz2003@gmail.com>
2022-10-27 19:48:08 +0330

    init

---
+++ test.py
@@ -0,0 +1,11 @@
+a=int(input('adad 1: '))
+b=int(input('adad 2: '))
+c=int(input('adad 3: '))
+if a<=b+c and b<=a+c and c<=a+b :
+    print('bale mosalas ast')
+else :
+    print('na mosalas nist')
+if a**2+c**2==b**2 or b**2+c**2==a**2 or a**2+b**2==c**2 :
+    print('bale qaem ast')
+else :
+    print('na qaem nist'
